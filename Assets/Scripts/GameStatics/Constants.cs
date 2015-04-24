93d0f1b4f4f3d8d7ca0f0f246b211a94724f402b
Lin Shengjie <linsj13@mails.tsinghua.edu.cn>
2015-04-24 21:19:13 +0800

    adjust the parameters

--- Assets/Scripts/GameStatics/Constants.cs
+++ Assets/Scripts/GameStatics/Constants.cs
@@ -5,9 +5,9 @@
 	public static readonly int[] BuildRounds = { 0, 0, 0, 0, 2, 3, 5, 3, 3, 2 };
 	public static readonly string[] ChineseNames = { "基地", "据点", "矿场", "油田", "潜艇", "驱逐舰", "航母", "运输舰", "战斗机", "侦察机" };
 	public static readonly int[] Costs = { 0, 0, 0, 0, 7, 14, 30, 16, 14, 10 };
-	public static readonly int[] MaxHP = { 700, 300, 0, 0, 35, 70, 120, 80, 50, 50 };
+	public static readonly int[] MaxHP = { 1000, 300, 0, 0, 35, 70, 120, 80, 50, 50 };
 	public static readonly int[] Population = { 0, 0, 0, 0, 2, 3, 4, 1, 3, 1 };
-	public static readonly int[] Speed = { 0, 0, 0, 0, 6, 7, 5, 11, 9, 10 };
+	public static readonly int[] Speed = { 0, 0, 0, 0, 6, 7, 5, 11, 9, 12 };
 	public static readonly string[] TypeNames = { "Base", "Fort", "Mine", "Oilfield", "Submarine", "Destroyer", "Carrier", "Cargo", "Fighter", "Scout" };
 
 	public static class Score
